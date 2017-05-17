816a8d1bd21afe4880c593f9ad3989de24d40301
pedroteosousa <pedroteosousa@gmail.com>
2017-05-17 01:11:34 -0300

    initial commit

---
+++ bot.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api')
+const fs = require('fs')
+
+const token = fs.readFileSync('token.txt', 'utf8')
+
+const bot = new TelegramBot(token, {polling: true})
+
+function getResponse(text) {
+    const pattern = /\bSussu\b/g
+    if (text[text.length-1] == '?') return "7"
+    else if ((match = pattern.exec(text)) != null) {
+    	return  "AI SUSSU!"
+    }
+    else if (text[text.length-1] == '!' || text[text.length-1] == '.') return "O cara é bom!"
+    return ""
+}
+
+bot.on('message', (msg) => {
+  if (msg.text != undefined && getResponse(msg.text).length) bot.sendMessage(msg.chat.id, getResponse(msg.text))
+})
