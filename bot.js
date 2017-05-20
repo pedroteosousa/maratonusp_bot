1a866073c11a9b40a6425ea1886c300f43184412
pedroteosousa <pedroteosousa@gmail.com>
2017-05-20 17:57:02 -0300

    fixed random and not calling getResponse twice due to different returns

--- bot.js
+++ bot.js
@@ -8,13 +8,13 @@ const bot = new TelegramBot(token, {polling: true})
 function getResponse(text) {
     const pattern = /\bSussu\b/g
     if (text[text.length-1] == '?') {
-        if (Math.rand() > .1)
+        if (Math.random() > .1)
             return "";
         return "7"
     } else if ((match = pattern.exec(text)) != null) {
     	return  "AI SUSSU!"
     } else if (text[text.length-1] == '!' || text[text.length-1] == '.') {
-        if (Math.rand() > .1)
+        if (Math.random() > .1)
             return "";
         return "O cara é bom!"
     }
@@ -22,5 +22,6 @@ function getResponse(text) {
 }
 
 bot.on('message', (msg) => {
-  if (msg.text != undefined && getResponse(msg.text).length) bot.sendMessage(msg.chat.id, getResponse(msg.text))
+    var response = getResponse(msg.text)
+    if (msg.text != undefined && response.length) bot.sendMessage(msg.chat.id, response)
 })
